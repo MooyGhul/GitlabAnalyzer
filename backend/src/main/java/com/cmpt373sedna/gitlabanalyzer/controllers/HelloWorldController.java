a64e6a1599e5653082dd2ee19bdae8997a88f80b
Richard Fu <rqfu@sfu.ca>
2021-02-01 01:32:49 -0800

    Resolves #2, add Postgres DB

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/controllers/HelloWorldController.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/controllers/HelloWorldController.java
@@ -1,17 +1,28 @@
 package com.cmpt373sedna.gitlabanalyzer.controllers;
 
 import com.cmpt373sedna.gitlabanalyzer.model.HelloWorld;
+import com.cmpt373sedna.gitlabanalyzer.repository.HelloWorldRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
+    private final HelloWorldRepository repository;
+
     @GetMapping("/")
     public String helloWorld() {
-        HelloWorld helloWorld = HelloWorld.builder()
-                .hello("hello")
-                .world("world")
-                .build();
+        HelloWorld helloWorld = repository.findById("hello").orElse(null);
+
+        if (helloWorld == null) {
+            helloWorld = repository.save(HelloWorld.builder()
+                    .hello("hello")
+                    .world("world")
+                    .build());
+        }
 
         return helloWorld.getHello() + helloWorld.getWorld();
     }
