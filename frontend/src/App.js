21deb3f0f57267a852e380558d3d7755092235d8
katelynk <katelynk@sfu.ca>
2021-02-02 00:27:14 -0800

    Add a header component with css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import ScoreBoard from './components/ScoreBoard';
 import { Student } from './mockInfo';
-
+import Header from './components/Header';
 
 function App() {
   
   return (
     <div>
-
+      <Header
+        pageTitle="Overview"
+      />
+      
       <ScoreBoard 
         totalCommit={Student.totalCommit} 
         TotalMR={Student.TotalMR} 
