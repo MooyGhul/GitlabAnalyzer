b969c3cf3c73542a040353ec7632fdd26fd71894
katelynk <katelynk@sfu.ca>
2021-02-02 16:47:15 -0800

    Add a css library

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 Header.js
 
 import React from 'react';  
+import styles from "../style/header.module.css"
 
 const Header = (pageTitle) => {
     return <h1 className={styles.header}>{pageTitle.pageTitle}</h1>
