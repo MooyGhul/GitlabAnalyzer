bbe890c1c507c65cc2e97540fdfe3df7553361ee
gss33 <gss33@sfu.ca>
2021-02-04 17:29:18 -0800

    Fill Repository list fields with data from links

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/Application.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/Application.java
@@ -1,10 +1,13 @@
 package com.cmpt373sedna.gitlabanalyzer;
 
 import com.cmpt373sedna.gitlabanalyzer.model.Repository;
+import org.json.JSONObject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class Application {
@@ -18,7 +21,15 @@ public class Application {
         return args -> {
             String URL = "http://cmpt373-1211-14.cmpt.sfu.ca:8929/api/v4/projects/";
             Repository repo = new Repository(URL);
-//            System.out.println(repo.getRepoId());
+//            ArrayList<String> members = repo.getMembers();
+//            ArrayList<JSONObject> mergeRequests = repo.getMergeRequests();
+//            ArrayList<JSONObject> issues = repo.getIssues();
+            ArrayList<JSONObject> branches = repo.getBranches();
+
+//            members.forEach(System.out::println);
+//            mergeRequests.forEach(System.out::println);
+//            issues.forEach(System.out::println);
+            branches.forEach(System.out::println);
         };
     }
 }
--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/model/Repository.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/model/Repository.java
@@ -1,6 +1,8 @@
 package com.cmpt373sedna.gitlabanalyzer.model;
 
 import com.cmpt373sedna.gitlabanalyzer.controllers.Extractor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.persistence.Id;
 import java.util.ArrayList;
@@ -9,20 +11,59 @@ public @lombok.Data class Repository {
     private @Id String repoId;
     private String repoName;
     private int[] weights;
-    private ArrayList<String> mergeRequests;
-    private ArrayList<String> issues;
-    private ArrayList<String> branches;
+    private ArrayList<JSONObject> mergeRequests;
+    private ArrayList<JSONObject> issues;
+    private ArrayList<JSONObject> branches;
     private ArrayList<String> members;
 
     public Repository(String url) {
+        this.mergeRequests = new ArrayList<JSONObject>();
+        this.issues = new ArrayList<JSONObject>();
+        this.branches = new ArrayList<JSONObject>();
+        this.members = new ArrayList<String>();
+
         Extractor e = new Extractor();
         // 0: id.toString(), 1: name, 2:mergeRequestLink, 3:issuesLink, 4:repoBranchesLink, 5:membersLink
         String[] links = e.getBasicRepoLinks();
         this.repoId = links[0];
         this.repoName = links[1];
+
+        JSONArray jsonMergeRequests = e.getMergeRequests(links[2]);
+        jsonMergeRequests.forEach(mr -> {
+            this.mergeRequests.add((JSONObject) mr);
+        });
+
+        JSONArray jsonIssues = e.getIssues(links[3]);
+        jsonIssues.forEach(issue -> {
+            this.issues.add((JSONObject) issue);
+        });
+
+        JSONArray jsonBranches = e.getBranches(links[4]);
+        jsonBranches.forEach(branch -> {
+            this.branches.add((JSONObject) branch);
+        });
+
+        JSONArray jsonMembers = e.getRepoMembers(links[5]);
+        jsonMembers.forEach(member -> {
+            JSONObject obj = (JSONObject) member;
+            this.members.add((String) obj.get("username"));
+        });
+
     }
 
-    public String getRepoId() {
-        return repoId;
+    public ArrayList<JSONObject> getMergeRequests() {
+        return mergeRequests;
+    }
+
+    public ArrayList<JSONObject> getIssues() {
+        return issues;
+    }
+
+    public ArrayList<JSONObject> getBranches() {
+        return branches;
+    }
+
+    public ArrayList<String> getMembers() {
+        return members;
     }
 }
