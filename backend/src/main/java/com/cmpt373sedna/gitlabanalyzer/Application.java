8f5d3e6afb1804f735175b6e917ddc454a7c1ba5
gss33 <gss33@sfu.ca>
2021-02-04 16:42:00 -0800

    Add console log test for Repository object

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/Application.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/Application.java
@@ -1,7 +1,10 @@
 package com.cmpt373sedna.gitlabanalyzer;
 
+import com.cmpt373sedna.gitlabanalyzer.model.Repository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,12 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run() throws Exception {
+        return args -> {
+            String URL = "http://cmpt373-1211-14.cmpt.sfu.ca:8929/api/v4/projects/";
+            Repository repo = new Repository(URL);
+//            System.out.println(repo.getRepoId());
+        };
+    }
 }
