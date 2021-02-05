01993f92f66977eb193557e46fa5ed22bd2aaf0a
katelynk <katelynk@sfu.ca>
2021-02-04 17:28:53 -0800

    Replace pageTitle with props in Headercomponent

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import styles from "../style/header.module.css"
 
-const Header = (pageTitle) => {
-    return <h1 className={styles.header}>{pageTitle.pageTitle}</h1>
+const Header = (props) => {
+    return <h1 className={styles.header}>{props.pageTitle}</h1>
 }
 
 export default Header;
