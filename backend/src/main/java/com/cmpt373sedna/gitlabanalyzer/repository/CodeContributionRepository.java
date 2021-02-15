923ce54895227879b29e130adb857617bc9fe26e
olehs <olehs@sfu.ca>
2021-02-14 20:03:56 -0800

    Add typing to CodeContributionRepository

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/CodeContributionRepository.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/CodeContributionRepository.java
@@ -1,6 +1,7 @@
 package com.cmpt373sedna.gitlabanalyzer.repository;
 
+import com.cmpt373sedna.gitlabanalyzer.model.CodeContributionHistory;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CodeContributionRepository extends CrudRepository {
+public interface CodeContributionRepository extends CrudRepository<CodeContributionHistory, Integer> {
 }
