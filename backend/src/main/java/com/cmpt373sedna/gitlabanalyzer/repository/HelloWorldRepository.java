ec36b5bd1fa7b6fc07d8b79db7596ad5cabcd8c7
Richard Fu <rqfu@sfu.ca>
2021-02-15 15:14:42 -0800

    Delete HelloWorld classes since project has real code now

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/HelloWorldRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cmpt373sedna.gitlabanalyzer.repository;
-
-import com.cmpt373sedna.gitlabanalyzer.model.HelloWorld;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloWorldRepository extends CrudRepository<HelloWorld, String> {
-}
