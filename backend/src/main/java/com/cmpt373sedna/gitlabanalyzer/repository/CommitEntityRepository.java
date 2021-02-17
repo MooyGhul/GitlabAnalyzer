995dab63f68d57b05b4be8cd112fedea2c3a9b8b
gss33 <gss33@sfu.ca>
2021-02-16 21:42:19 -0800

    Add missing repository annotations

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/CommitEntityRepository.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/CommitEntityRepository.java
@@ -2,6 +2,8 @@ package com.cmpt373sedna.gitlabanalyzer.repository;
 
 import com.cmpt373sedna.gitlabanalyzer.model.CommitEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommitEntityRepository extends CrudRepository<CommitEntity, Integer> {
 }
--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/IssueEntityRepository.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/IssueEntityRepository.java
@@ -2,6 +2,8 @@ package com.cmpt373sedna.gitlabanalyzer.repository;
 
 import com.cmpt373sedna.gitlabanalyzer.model.IssueEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IssueEntityRepository extends CrudRepository<IssueEntity, Integer> {
 }
