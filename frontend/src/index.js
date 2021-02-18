5c54f7874b72a04c5c31ccfe4509713aa6c9a107
panwangp <richard_pan@sfu.ca>
2021-02-17 20:06:24 -0800

    Fix unused variable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './components/Login';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
