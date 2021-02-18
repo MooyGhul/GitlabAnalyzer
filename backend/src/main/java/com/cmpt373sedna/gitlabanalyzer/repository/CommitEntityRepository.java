dcddf973653fe88e780a90f9679d697411b9d21c
gss33 <gss33@sfu.ca>
2021-02-18 15:11:10 -0800

    Update function calls in CodeContributionController

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/CommitEntityRepository.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/CommitEntityRepository.java
@@ -1,7 +1,6 @@
 package com.cmpt373sedna.gitlabanalyzer.repository;
 
 import com.cmpt373sedna.gitlabanalyzer.model.CommitEntity;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
