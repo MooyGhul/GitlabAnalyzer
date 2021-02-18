a81427e86fbe3c7d41e8631b57d282a642f80ba6
panwangp <richard_pan@sfu.ca>
2021-02-17 18:56:59 -0800

    Some previous fix was lost when solving merge conflict, get those fix back

--- frontend/src/components/Charts.js
+++ frontend/src/components/Charts.js
@@ -7,62 +7,62 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 let contributions = [
   {
-    year: "2021-1-25",
+    "year": "2021-1-25",
     "MRDaily": 20,
     "CommitDaily": 5,
   },
   {
-    year: "2021-1-26",
+    "year": "2021-1-26",
     "MRDaily": 12,
     "CommitDaily": 3,
   },
   {
-    year: "2021-1-27",
+    "year": "2021-1-27",
     "MRDaily": 0,
     "CommitDaily": 0,
   },
   {
-    year: "2021-1-28",
+    "year": "2021-1-28",
     "MRDaily": 16,
     "CommitDaily": 2,
   },
   {
-    year: "2021-1-29",
+    "year": "2021-1-29",
     "MRDaily": 0,
     "CommitDaily": 1,
   },
   {
-    year: "2021-1-30",
+    "year": "2021-1-30",
     "MRDaily": 0,
     "CommitDaily": 6,
   },
   {
-    year: "2021-1-31",
+    "year": "2021-1-31",
     "MRDaily": 7,
     "CommitDaily": 0,
   },
   {
-    year: "2021-2-1",
+    "year": "2021-2-1",
     "MRDaily": 0,
     "CommitDaily": 0,
   },
   {
-    year: "2021-2-2",
+    "year": "2021-2-2",
     "MRDaily": 35,
     "CommitDaily": 2,
   },
   {
-    year: "2021-2-3",
+    "year": "2021-2-3",
     "MRDaily": 2,
     "CommitDaily": 7,
   },
   {
-    year: "2021-2-4",
+    "year": "2021-2-4",
     "MRDaily": 0,
     "CommitDaily": 3,
   },
   {
-    year: "2021-2-5",
+    "year": "2021-2-5",
     "MRDaily": 1,
     "CommitDaily": 0,
   }
@@ -112,7 +112,7 @@ export default function Charts () {
       contributionsDataProp = filterData(contributionsData,startDate,endDate);
       commentsDataProp = filterData(commentsData,startDate,endDate);
     }
- 
+
     return (
         <div>
           <br/>
@@ -134,7 +134,6 @@ export default function Charts () {
               onChange={date => setEndDate(date)} 
               dateFormat='MM/dd/yyyy'
               minDate={startDate}
-              filterDate={date => date.getDay() !== 6 && date.getDay !== 0}
               isClearable
               showYearDropdown
               scrollableMonthYearDropdown
