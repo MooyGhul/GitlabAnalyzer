c78e2903ca6e1dd05b23736f89133c0cf16651b7
dpurkays <dula_purkaystha@sfu.ca>
2021-02-20 20:16:36 -0800

    Update OverviewPage.js to match develop's App.js

--- frontend/src/OverviewPage.js
+++ frontend/src/OverviewPage.js
@@ -1,17 +1,19 @@
 import ScoreBoard from './components/ScoreBoard';
 import Header from './components/Header';
+import Charts from "./components/Charts";
+import './App.css';
 
 function OverviewPage(props) {
 
   return (
-    <div>
-      <Header
-        pageTitle="Overview"
-      />
-
-      <ScoreBoard />
-
-    </div>
+      <div>
+          <Header
+              pageTitle="Overview"
+          />
+          <ScoreBoard />
+          <br/><br/><br/>
+          <Charts />
+      </div>
   );
 }
 
