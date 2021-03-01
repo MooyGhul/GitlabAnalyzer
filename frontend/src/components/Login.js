28f9f0f6119c542ac23e84a9f401182a7d70d276
katelynk <katelynk@sfu.ca>
2021-03-01 12:36:25 -0800

    Move makeStyle to a separate file to separate logic from styling

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -8,7 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';  
 import Box from '@material-ui/core/Box';  
 import logo from '../logo/gitlab_analyzer.png';
-
+import {useStyles} from '../style/LoginStyle'
 
 
 function Login() {
@@ -38,59 +38,14 @@ function Login() {
         event.preventDefault();
         login();
     }
-
-    const useStyles = makeStyles((theme) => ({        
-        h2:{ 
-            marginTop: '30%',
-            marginBottom: '15%', 
-        },
-
-        formBox:{ 
-            width:'25%',
-            height:'55%', 
-            position: 'absolute', 
-            left: '50%', 
-            top: '50%',
-            transform: 'translate(-50%, -50%)',
-            textAlign: 'center',
-            
-        },
         
-        customButton:{
-            marginTop:'15%',
-            width:'50%',
-            backgroundImage: 'linear-gradient(to bottom right,rgb(100,7,254), rgb(214,35,113))',
-            backgroundColor: 'rgb(92,58,171)',     
-        },
-
-        customTextField: {
-            width: '70%'
-        },         
-        
-        logo: {
-            width:"50%",
-            position: 'absolute',
-            left: '50%', 
-            top: '15%',
-            transform: 'translate(-50%, -50%)', 
-
-        }
-      }));
-      
-      const defaultProps = {
-        bgcolor: 'background.paper',
-        borderColor: 'rgb(195,195,195)',
-        m: 1,
-        border: 1, 
-      };
-    
     const classes = useStyles();
 
     return(
         <div>
             <Header pageTitle='Gitlab Analyzer' />          
             
-             <Box className={classes.formBox} borderRadius={16} {...defaultProps}  boxShadow={8}  > 
+             <Box className={classes.formBox} borderRadius={16} boxShadow={8}  > 
                 <img src={logo} alt="Logo" className={classes.logo}/>      
                 <h2 className={classes.h2}>LOGIN</h2>
                 <form noValidate autoComplete='off' onSubmit={loginHandler}>
@@ -99,8 +54,7 @@ function Login() {
                     <TextField id='password' classes={{root: classes.customTextField}} label='Password' value={user.password} fullWidth onChange={e=> setUser({...user, password: e.target.value})}/>                
                     <Button classes={{root: classes.customButton}} variant='contained'  type ='submit' color='secondary'> Log in</Button>    
                 </form>  
-            </Box>
-             
+            </Box>             
         </div>
     );
 }
