44f9be93a1d980c631be3655215b11c25c06039d
KSSidhu <kirat.sidhu.sw@gmail.com>
2021-03-01 10:22:53 -0800

    Remove export warnings and add new styles pages

---
+++ frontend/src/style/ScoreBoardStyles.js
@@ -0,0 +1,14 @@
+const styles = {
+  buttons: {
+    marginTop: "80px",
+    display: "flex",
+    flexDirection: "column",
+    justify: "space-evenly",
+  },
+
+  button: {
+    margin: "15px",
+  },
+};
+
+export default styles;
