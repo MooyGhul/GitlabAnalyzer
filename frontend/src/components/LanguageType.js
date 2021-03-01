82a898381e78ba98dd6a5f0cf35fbdcbdb258e1a
KSSidhu <kirat.sidhu.sw@gmail.com>
2021-03-01 10:24:11 -0800

    Rebuild ScoreBoard and LanguageType with Material UI

--- frontend/src/components/LanguageType.js
+++ frontend/src/components/LanguageType.js
@@ -1,27 +1,65 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { withStyles } from "@material-ui/core";
+import styles from "../style/LanguageTypeStyles";
 
-export default function LanguageType (props) {
+const LanguageType = (props) => {
+  // TO DO :
+  // When backend is ready, either GREY OUT "PLEASE CHOOSE LANGUAGE"
+  // or calculate overall submission when ppl choose "PLEASE CHOOSE LANGUAGE"
 
-    // TO DO :
-    // When backend is ready, either GREY OUT "PLEASE CHOOSE LANGUAGE"
-    // or calculate overall submission when ppl choose "PLEASE CHOOSE LANGUAGE"
+  const { classes } = props;
+  const options = ["JS", "C++", "Java", "Python"];
 
-    const handleFile = (event)=>{
-        props.onChange(event.target.value);
-    };
-    return (
-        <div>
-            <select 
-                id="language"
-                onChange={handleFile}
-            >
-              <option value="Empty" >PLEASE CHOOSE LANGUAGE</option>
-              <option value="JS">JS</option>
-              <option value="C++">C++</option>
-              <option value="Java">Java</option>
-              <option value="Python">Python</option>
-              
-            </select>
-        </div>
-    );
-}
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleFile = (event, index) => {
+    props.onChange(options[index]);
+    setSelectedIndex(index);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        className={classes.languageBtn}
+      >
+        File Type: {options[selectedIndex]}
+      </Button>
+      <Menu
+        id="language-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        variant="menu"
+      >
+        {options.map((option, index) => (
+          <MenuItem
+            key={option}
+            selected={index === selectedIndex}
+            onClick={(event) => handleFile(event, index)}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+};
+
+export default withStyles(styles)(LanguageType);
