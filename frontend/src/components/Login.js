62869af13b3b6920e1401b6bf26ce3be7ba86ec0
katelynk <katelynk@sfu.ca>
2021-02-28 21:52:11 -0800

    Center the login form and apply material UI

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AdminUser } from '../mockDataDir/mockAdminUser';
-import Authentication from '../Authentication';
-import styles from '../style/Login.module.css';
+import Authentication from '../Authentication'; 
 import Header from './Header'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
-//Note: Use AdminUser's username and password from mockInfo to login
 function Login() {
     const history = useHistory();
     const [user, setUser] = useState({name:'', password:''});
@@ -34,24 +37,45 @@ function Login() {
         login(user);
     }
 
+    const useStyles = makeStyles((theme) => ({
+        root: { 
+            fontFamily:'PT Sans, sans-serif' 
+        },  
+
+        formContainer:{
+            backgroundColor:'#7553FF',   
+            width:'30%',
+            position: 'absolute', 
+            left: '50%', 
+            top: '50%',
+            transform: 'translate(-50%, -50%)'
+        },
+
+        textfield: {
+            display: 'block',
+            marginBottom: '5%', 
+        }
+      })
+      )
+      ;
+    
+    const classes = useStyles();
+
     return(
-        <div>
-            <Header pageTitle="Gitlab Analyzer" />
-            <h2>Login</h2>
-            <form className={styles.form} onSubmit={loginHandler}>
-                <h3>{errorMsg}</h3>
-                <label className={styles.label}>
-                    Username
-                    <input type ='text' value={user.name} onChange={e=> setUser({...user, name: e.target.value})} />
-                </label>
-                <label className={styles.label}>
-                    Password
-                    <input type ='password' value={user.password} onChange={e=> setUser({...user, password: e.target.value})} />
-                </label>
-                    <button className={styles.button} type ='submit'>
-                        Login
-                    </button>
-            </form>
+        <div className={classes.root} >
+            {/* <Header pageTitle='Gitlab Analyzer' /> */}          
+           
+             <Container className={classes.formContainer}>
+                <h2 className={classes.h2}>Login</h2>
+                <form noValidate autoComplete='off' onSubmit={loginHandler}>
+                    <h3>{errorMsg}</h3>
+                    <TextField id='standard-basic' className={classes.textfield} label='Username' value={user.name} fullWidth onChange={e=> setUser({...user, name: e.target.value})}/>
+                    <TextField id='standard-basic' className={classes.textfield} label='Password' alue={user.password} fullWidth onChange={e=> setUser({...user, password: e.target.value})}/>                
+                    <Button variant='contained'  type ='submit' color='secondary'> Log in</Button>
+    
+                </form>  
+            </Container>
+             
         </div>
     );
 }
