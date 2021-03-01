eab338a306a26afee2407b18341ec08302acacc1
KSSidhu <kirat.sidhu.sw@gmail.com>
2021-03-01 11:56:57 -0800

    Add defaults

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -4,19 +4,21 @@ import ScoreBoard from "./ScoreBoard";
 import styles from "../style/BannerStyles";
 
 const Banner = (props) => {
-  const { classes } = props;
+  const { classes, avatar_url, membername } = props;
   return (
     <Grid container justify={"space-evenly"} className={classes.container}>
-      <Grid item className={classes.profile}>
+      <Grid item md={6} sm={12} className={classes.profile}>
         <Avatar
-          alt=""
-          src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"
+          alt="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"
+          src={avatar_url}
           className={classes.large}
         />
-        <Typography className={classes.details}>Batman</Typography>
+        <Typography className={classes.details}>
+          {membername ? membername : "Batman"}
+        </Typography>
       </Grid>
 
-      <Grid item>
+      <Grid item md={6} sm={12}>
         <ScoreBoard />
       </Grid>
     </Grid>
