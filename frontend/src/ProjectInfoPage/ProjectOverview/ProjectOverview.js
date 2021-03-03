f028c02ac19c62db10335c55ed11fc23e2792d73
katelynk <katelynk@sfu.ca>
2021-03-03 03:11:07 -0800

    Fetch project-specific data to ProjectOverview.js

--- frontend/src/ProjectInfoPage/ProjectOverview/ProjectOverview.js
+++ frontend/src/ProjectInfoPage/ProjectOverview/ProjectOverview.js
@@ -1,13 +1,29 @@
 import styles from './ProjectOverview.module.css'
 import MemberList from './MemberList'  
 import { useLocation } from 'react-router-dom';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios'; 
+
 
 function ProjectOverview(){   
     const location = useLocation();
+    const projectID = location.state.id
+    const [data, setData] = useState ([]);
+    useEffect(() => {    
+        
+          const fetchData = async () => {
+            const result = await axios.get(`http://localhost:8080/project/${projectID}/members`)
+            
+            setData(result.data);
+          };
+          fetchData();
+        }, []); 
+    console.log(data);
+ 
 
     return(
         <div className={styles.ProjectOverview}>
-            <img className={styles.exportIcon} src="https://img.icons8.com/color/48/000000/export-pdf.png" alt="export"/>      
+            <img className={styles.exportIcon} src="https://img.icons8.com/color/48/000000/export-pdf.png" alt="export"/>         
             {console.log(location.state.id)}
             <MemberList/> 
             
