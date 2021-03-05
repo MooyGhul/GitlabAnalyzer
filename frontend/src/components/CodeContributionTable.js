e0bc73e9de09f4052a74f1f2844c45cb4d49c847
dpurkays <dula_purkaystha@sfu.ca>
2021-03-04 18:06:17 -0800

    Add static table header and extract header titles

--- frontend/src/components/CodeContributionTable.js
+++ frontend/src/components/CodeContributionTable.js
@@ -1,42 +1,63 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import {CommitDetails} from '../mockDataDir/mockCommitDetails';
 import {MRDetails} from '../mockDataDir/mockMRDetails';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 500,
+  },
+});
 
 const rows = [].concat(MRDetails, CommitDetails);
+const columns = [
+  {id: 'type', label: 'Type'},
+  {id: 'date', label: 'Date'},
+  {id: 'details', label: 'Details'},
+  {id: 'score', label: 'Score'},
+
+]
 console.log(rows);
 
 function CodeContributionTable () {
-
+  const classes = useStyles();
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Type</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Details</TableCell>
-            <TableCell>Score</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell>{row.type}</TableCell>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.score}</TableCell>
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell key={column.id}>
+                  {column.label}
+                </TableCell>
+              ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell>{row.type}</TableCell>
+                <TableCell>{row.date}</TableCell>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.score}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
 
--- frontend/src/mockDataDir/mockMRDetails.js
+++ frontend/src/mockDataDir/mockMRDetails.js
@@ -14,5 +14,85 @@ export const MRDetails = [
     date: "2021-1-25",
     name: 'Create Database',
     score: 60
+  },
+  {
+    date: "2021-1-25",
+    name: 'Fix the Navigation Bar',
+    score: 25
+
+  },
+  {
+    date: "2021-1-25",
+    name: 'Apply Material UI to Overview',
+    score: 30
+  },
+  {
+    date: "2021-1-25",
+    name: 'Create Database',
+    score: 60
+  },
+  {
+    date: "2021-1-25",
+    name: 'Fix the Navigation Bar',
+    score: 25
+
+  },
+  {
+    date: "2021-1-25",
+    name: 'Apply Material UI to Overview',
+    score: 30
+  },
+  {
+    date: "2021-1-25",
+    name: 'Create Database',
+    score: 60
+  },
+  {
+    date: "2021-1-25",
+    name: 'Fix the Navigation Bar',
+    score: 25
+
+  },
+  {
+    date: "2021-1-25",
+    name: 'Apply Material UI to Overview',
+    score: 30
+  },
+  {
+    date: "2021-1-25",
+    name: 'Create Database',
+    score: 60
+  },
+  {
+    date: "2021-1-25",
+    name: 'Fix the Navigation Bar',
+    score: 25
+
+  },
+  {
+    date: "2021-1-25",
+    name: 'Apply Material UI to Overview',
+    score: 30
+  },
+  {
+    date: "2021-1-25",
+    name: 'Create Database',
+    score: 60
+  },
+  {
+    date: "2021-1-25",
+    name: 'Fix the Navigation Bar',
+    score: 25
+
+  },
+  {
+    date: "2021-1-25",
+    name: 'Apply Material UI to Overview',
+    score: 30
+  },
+  {
+    date: "2021-1-25",
+    name: 'Create Database',
+    score: 60
   }
 ];
