5b5a36c0bdf18e1a4911a1de4489a06991a42557
panwangp <richard_pan@sfu.ca>
2021-03-05 16:29:18 -0800

    change REST mapping a little bit

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/controllers/MemberController.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/controllers/MemberController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/project/{projectId}")
+@RequestMapping("/project/{projectId}/member")
 public class MemberController {
 
     @Autowired
