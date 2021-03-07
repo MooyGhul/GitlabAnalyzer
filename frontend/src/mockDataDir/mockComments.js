0058131be2d2e8221c861a70fd47a6bc757884ed
gss33 <gss33@sfu.ca>
2021-03-07 15:09:50 -0800

    Add mock data and styling

---
+++ frontend/src/mockDataDir/mockComments.js
@@ -0,0 +1,119 @@
+const Comments = [
+    {
+        "id": 53,
+        "type": null,
+        "body": "mentioned in commit 5c09fca0eb90c8cf8ed44a24ced87c425fdea553\n\n *By rqfu on 2021-02-01T17:37:49 (imported from GitLab)*",
+        "attachment": null,
+        "author": {
+            "id": 1,
+            "name": "Administrator",
+            "username": "root",
+            "state": "active",
+            "avatar_url": "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+            "web_url": "http://cmpt373-1211-14.cmpt.sfu.ca:8929/root"
+        },
+        "created_at": "2021-02-02T01:37:49.188Z",
+        "updated_at": "2021-02-02T01:37:49.191Z",
+        "system": true,
+        "noteable_id": 5,
+        "noteable_type": "MergeRequest",
+        "resolvable": false,
+        "confidential": false,
+        "noteable_iid": 5,
+        "commands_changes": {}
+    },
+    {
+        "id": 54,
+        "type": null,
+        "body": "marked this merge request as **ready**\n\n *By rqfu on 2021-02-01T17:37:26 (imported from GitLab)*",
+        "attachment": null,
+        "author": {
+            "id": 1,
+            "name": "Administrator",
+            "username": "root",
+            "state": "active",
+            "avatar_url": "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+            "web_url": "http://cmpt373-1211-14.cmpt.sfu.ca:8929/root"
+        },
+        "created_at": "2021-02-02T01:37:26.658Z",
+        "updated_at": "2021-02-02T01:37:26.662Z",
+        "system": true,
+        "noteable_id": 5,
+        "noteable_type": "MergeRequest",
+        "resolvable": false,
+        "confidential": false,
+        "noteable_iid": 5,
+        "commands_changes": {}
+    },
+    {
+        "id": 55,
+        "type": null,
+        "body": "approved this merge request\n\n *By gss33 on 2021-02-01T17:36:13 (imported from GitLab)*",
+        "attachment": null,
+        "author": {
+            "id": 1,
+            "name": "Administrator",
+            "username": "root",
+            "state": "active",
+            "avatar_url": "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+            "web_url": "http://cmpt373-1211-14.cmpt.sfu.ca:8929/root"
+        },
+        "created_at": "2021-02-02T01:36:13.838Z",
+        "updated_at": "2021-02-02T01:36:13.842Z",
+        "system": true,
+        "noteable_id": 5,
+        "noteable_type": "MergeRequest",
+        "resolvable": false,
+        "confidential": false,
+        "noteable_iid": 5,
+        "commands_changes": {}
+    },
+    {
+        "id": 52,
+        "type": null,
+        "body": "Looks good to me\n\n *By gss33 on 2021-02-01T17:36:11 (imported from GitLab)*",
+        "attachment": null,
+        "author": {
+            "id": 1,
+            "name": "Administrator",
+            "username": "root",
+            "state": "active",
+            "avatar_url": "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+            "web_url": "http://cmpt373-1211-14.cmpt.sfu.ca:8929/root"
+        },
+        "created_at": "2021-02-02T01:36:11.790Z",
+        "updated_at": "2021-02-02T01:36:11.790Z",
+        "system": false,
+        "noteable_id": 5,
+        "noteable_type": "MergeRequest",
+        "resolvable": false,
+        "confidential": false,
+        "noteable_iid": 5,
+        "commands_changes": {}
+    },
+    {
+        "id": 56,
+        "type": null,
+        "body": "added 2 commits\n\n<ul><li>6db219b4 - Add class to represent Student Data</li><li>bdede2d9 - Merge branch &#39;develop&#39; into 3-create-student-class</li></ul>\n\n[Compare with previous version](/373-2021-1-Sedna/gitlabanalyzer/-/merge_requests/5/diffs?diff_id=59594&start_sha=4d7dd8925d88281a5a76e785fcb29979ab5f9fee)\n\n *By syedm on 2021-02-01T16:56:02 (imported from GitLab)*",
+        "attachment": null,
+        "author": {
+            "id": 1,
+            "name": "Administrator",
+            "username": "root",
+            "state": "active",
+            "avatar_url": "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+            "web_url": "http://cmpt373-1211-14.cmpt.sfu.ca:8929/root"
+        },
+        "created_at": "2021-02-02T00:56:02.596Z",
+        "updated_at": "2021-02-02T00:56:02.602Z",
+        "system": true,
+        "noteable_id": 5,
+        "noteable_type": "MergeRequest",
+        "resolvable": false,
+        "confidential": false,
+        "noteable_iid": 5,
+        "commands_changes": {}
+    }
+];
+
+export default Comments;
