0b49a3d47b9296fa96a66ed4a1e97827c1d5253c
gss33 <gss33@sfu.ca>
2021-03-06 17:52:05 -0800

    Combine Header and Banner on OverviewPage

--- frontend/src/OverviewPage.js
+++ frontend/src/OverviewPage.js
@@ -1,39 +1,35 @@
 import Banner from "./components/Banner";
-import Header from './components/Header';
+import Header from "./components/Header";
 import Charts from "./components/Charts";
-import DataFetching from './components/DataFetching';
-import './OverviewPage.css';
+import DataFetching from "./components/DataFetching";
+import "./OverviewPage.css";
 import React from "react";
-import {makeStyles} from "@material-ui/core/styles";
-import {Grid} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   grid: {
     width: "100%",
-    margin: "0px"
+    margin: "0px",
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
     background: theme.palette.success.light,
-  }
+  },
 }));
 
-function OverviewPage(props) {
+function OverviewPage() {
   const classes = useStyles();
   return (
-    <Grid container spacing={2} className = {classes.grid}>
-      <Grid item xs={12} >
-        <Header
-              pageTitle="Overview Test"
-        />
-      </Grid>
-      <Grid container >
+    <Grid container spacing={2} className={classes.grid}>
+      <Grid container>
+        <Header pageTitle="Overview Test" />
         <Banner />
       </Grid>
 
-      <Grid container >
+      <Grid container>
         <Charts />
       </Grid>
       <DataFetching />
