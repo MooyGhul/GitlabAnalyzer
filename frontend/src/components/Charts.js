c85a1fb056ac7d619242457953805220c87ca256
panwangp <richard_pan@sfu.ca>
2021-03-10 21:14:28 -0800

    put mock data into its own file

--- frontend/src/components/Charts.js
+++ frontend/src/components/Charts.js
@@ -3,72 +3,12 @@ import BarChart from "./CommentContribution";
 import StackedBarChart from "./CodeContribution";
 import StackedBarChartPanel from "./StackedBarChartPanel";
 import {Comments} from "../mockDataDir/mockCodeContri";
+import {Contributions} from "../mockDataDir/mockGraphContri";
 import Grid from '@material-ui/core/Grid';
 import "react-datepicker/dist/react-datepicker.css";
 import Calendar from "./Calendar";
 
-let contributions = [
-  {
-    "year": "2021-1-25",
-    "MRDaily": 20,
-    "CommitDaily": 5,
-  },
-  {
-    "year": "2021-1-26",
-    "MRDaily": 12,
-    "CommitDaily": 3,
-  },
-  {
-    "year": "2021-1-27",
-    "MRDaily": 0,
-    "CommitDaily": 0,
-  },
-  {
-    "year": "2021-1-28",
-    "MRDaily": 16,
-    "CommitDaily": 2,
-  },
-  {
-    "year": "2021-1-29",
-    "MRDaily": 0,
-    "CommitDaily": 1,
-  },
-  {
-    "year": "2021-1-30",
-    "MRDaily": 0,
-    "CommitDaily": 6,
-  },
-  {
-    "year": "2021-1-31",
-    "MRDaily": 7,
-    "CommitDaily": 0,
-  },
-  {
-    "year": "2021-2-1",
-    "MRDaily": 0,
-    "CommitDaily": 0,
-  },
-  {
-    "year": "2021-2-2",
-    "MRDaily": 35,
-    "CommitDaily": 2,
-  },
-  {
-    "year": "2021-2-3",
-    "MRDaily": 2,
-    "CommitDaily": 7,
-  },
-  {
-    "year": "2021-2-4",
-    "MRDaily": 0,
-    "CommitDaily": 3,
-  },
-  {
-    "year": "2021-2-5",
-    "MRDaily": 1,
-    "CommitDaily": 0,
-  }
-];
+let contributions = Contributions;
 
 const allKeys = ["MRDaily", "CommitDaily"];
 
