6a1fd9838f7b973f91729ab7bda82cd1319284c8
Manmeet Singh <msa203@sfu.ca>
2021-03-11 05:13:54 -0800

    Removed comments

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/MergeRequestDiffsVersionsRepository.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/MergeRequestDiffsVersionsRepository.java
@@ -10,11 +10,4 @@ import java.util.List;
 @Repository
 public interface MergeRequestDiffsVersionsRepository extends CrudRepository<MergeRequestDiffVersionsEntity, Integer> {
 
-    //List<MergeRequestDiffVersionsEntity> findAllByProjectIdAndMerge_request_iidAndAuthorName(int id, int merge_request_iid, String authorName);
-
-    /*
-    @Query(value="SELECT * FROM (SELECT Count(mr) AS mrCount, mr.merged_at FROM merge_request_entity mr WHERE mr.project_id = :projectId GROUP BY mr.merged_at) as mrCounts" +
-            "        LEFT JOIN (SELECT Count(c) AS commitCount, c.commit_date FROM commit_entity c WHERE c.project_id = :projectId GROUP BY c.commit_date) as commitCounts" +
-            "        ON mrCounts.merged_at = commitCounts.commit_date", nativeQuery = true)*/
-
 }
