00a27274d3f55e3bff09ed5195fd69b50fbb4b04
Richard Fu <rqfu@sfu.ca>
2021-03-13 23:53:18 -0800

    Add start/end date's and friendly name to config entity

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/model/ConfigEntity.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/model/ConfigEntity.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.ZonedDateTime;
 
 @Data
 @Builder
@@ -19,6 +20,9 @@ public class ConfigEntity {
     // ID Generation from https://stackoverflow.com/a/40177990
     @Id @GeneratedValue(generator="system-uuid") @GenericGenerator(name="system-uuid", strategy = "uuid")
     private String id;
+    private String name;
     private String token;
     private String url;
+    private ZonedDateTime startDate;
+    private ZonedDateTime endDate;
 }
