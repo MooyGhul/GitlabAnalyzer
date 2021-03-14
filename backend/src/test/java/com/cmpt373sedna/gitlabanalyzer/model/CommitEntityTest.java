0af041afdb8950c94dd4a84454d7215ca050a789
Oleh Sokolov <alex.falconson@gmail.com>
2021-03-14 16:18:03 -0700

    Add SDF format

--- backend/src/test/java/com/cmpt373sedna/gitlabanalyzer/model/CommitEntityTest.java
+++ backend/src/test/java/com/cmpt373sedna/gitlabanalyzer/model/CommitEntityTest.java
@@ -2,23 +2,30 @@ package com.cmpt373sedna.gitlabanalyzer.model;
 
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.Commit;
 
 import java.io.IOException;
-import java.time.Instant;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommitEntityTest {
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     @Test
     void fromGitlabJSON_parses_Commit_correctly() {
-        CommitEntity expected = CommitEntity.builder()
-                .commitId("ed899a2f4b50b4370feeea94676502b42383c746")
-                .projectId(3)
-                .commitName("Replace sanitize with escape once")
-                .author("Example User")
-                .commitDate(Instant.parse("2012-09-20T07:00:00Z"))
-                .build();
+        CommitEntity expected = null;
+        try {
+            expected = CommitEntity.builder()
+                    .commitId("ed899a2f4b50b4370feeea94676502b42383c746")
+                    .projectId(3)
+                    .commitName("Replace sanitize with escape once")
+                    .author("Example User")
+                    .commitDate(sdf.parse("2012-09-20T11:50:22+03:00").toInstant())
+                    .build();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         String jsonString = "";
         try {
