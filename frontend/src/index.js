0a308a91a8a89947b711e73bb501a5a303cf9c8d
panwangp <richard_pan@sfu.ca>
2021-03-13 16:37:01 -0800

    save before apply fontawesome npm, clean to reverse

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Navbar from './components/Navbar/Navbar';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
