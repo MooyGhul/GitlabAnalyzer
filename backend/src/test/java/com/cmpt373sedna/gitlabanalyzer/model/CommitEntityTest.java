ce7de114664ff4a956c2299156fb5b33de3d2faf
Oleh Sokolov <alex.falconson@gmail.com>
2021-03-14 15:48:13 -0700

    Add Commit Test

---
+++ backend/src/test/java/com/cmpt373sedna/gitlabanalyzer/model/CommitEntityTest.java
@@ -0,0 +1,34 @@
+package com.cmpt373sedna.gitlabanalyzer.model;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Commit;
+
+import java.io.IOException;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommitEntityTest {
+    @Test
+    void fromGitlabJSON_parses_Commit_correctly() {
+        CommitEntity expected = CommitEntity.builder()
+                .commitId("ed899a2f4b50b4370feeea94676502b42383c746")
+                .commitName("Replace sanitize with escape once")
+                .author("Example User")
+                .commitDate(Instant.parse("2012-09-20T07:00:00Z"))
+                .build();
+
+        String jsonString = "";
+        try {
+            jsonString = new String(this.getClass().getResourceAsStream("/json/gitlabApi/singleCommit.json").readAllBytes());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        JSONObject json = new JSONObject(jsonString);
+        CommitEntity actual = CommitEntity.fromGitlabJSON(json);
+
+        assertEquals(expected, actual);
+
+    }
+}
