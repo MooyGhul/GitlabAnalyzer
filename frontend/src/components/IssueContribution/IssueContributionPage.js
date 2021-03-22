a4a26a731d446a5074053431017be4425c4e1af6
katelynk <katelynk@sfu.ca>
2021-03-22 09:47:31 -0700

    Refactor IssueContributionPage.js and IssueContributionStyle.js to improve design consistency across contribution pages

--- frontend/src/components/IssueContribution/IssueContributionPage.js
+++ frontend/src/components/IssueContribution/IssueContributionPage.js
@@ -1,90 +1,107 @@
-import {Grid, TableContainer, Table, TableCell, TableHead, TableRow, TableBody, Typography} from '@material-ui/core';
+import {
+  Grid,
+  TableContainer,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableBody,
+  Typography,
+} from "@material-ui/core";
 import Banner from "../Banner";
-import Navbar from '../Navbar/Navbar';
-import React, { useEffect, useState} from 'react';
-import useStyles from '../../style/IssueContributionPageStyles'; 
-import Row from './IssueTableDropDown'
-import axios from 'axios';
-import {getGraphData} from '../../helper';
-import BarChart from '../Charts/BarChart';
-import BarChartProperties from '../Charts/BarChartProperties';
+import Navbar from "../Navbar/Navbar";
+import React, {useEffect, useState} from "react";
+import useStyles from "../../style/IssueContributionPageStyles";
+import Row from "./IssueTableDropDown";
+import axios from "axios";
+import {getGraphData} from "../../helper";
+import BarChart from "../Charts/BarChart";
+import BarChartProperties from "../Charts/BarChartProperties";
 import {useParams} from "react-router";
-import InnerNavBar from '../InnerNavBar'
+import InnerNavBar from "../InnerNavBar";
 
 const IssueContributionPage = () => {
-    const styles = useStyles(); 
+  const classes = useStyles();
 
-    const [issues, setIssues] = useState([]); 
-    const [graphData, setGraphData] = useState([]);
-    const {project_id, member_id} = useParams();
+  const [issues, setIssues] = useState([]);
+  const [graphData, setGraphData] = useState([]);
+  const { project_id, member_id } = useParams();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const issueResult = await axios.get(process.env.NODE_ENV === 'development' ?
-                `${process.env.REACT_APP_DEVHOST}/project/${project_id}/member/${member_id}/issues` :
-                `/project/${project_id}/member/${member_id}/issues`
-            );
-            setIssues(issueResult.data); 
-            const issueCounts = getGraphData(issueResult.data, "openedDate");
-            setGraphData(issueCounts);
-        };
-        fetchData().then(() => {
-            console.log("Successfully obtained issues");
-        }).catch((e) => {
-            console.log("Failed to obtain issues");
-            console.log(e);
-        });
-    }, [project_id, member_id, setGraphData]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const issueResult = await axios.get(
+        process.env.NODE_ENV === "development"
+          ? `${process.env.REACT_APP_DEVHOST}/project/${project_id}/member/${member_id}/issues`
+          : `/project/${project_id}/member/${member_id}/issues`
+      );
+      setIssues(issueResult.data);
+      const issueCounts = getGraphData(issueResult.data, "openedDate");
+      setGraphData(issueCounts);
+    };
+    fetchData()
+      .then(() => {
+        console.log("Successfully obtained issues");
+      })
+      .catch((e) => {
+        console.log("Failed to obtain issues");
+        console.log(e);
+      });
+  }, [project_id, member_id, setGraphData]);
 
-    return (
-        <Grid container>
-            <Grid container spacing={0}>
-              <Grid item xs={12} >
-                <Navbar />
-              </Grid>
-              <Grid item xs={12} >
-                <Banner memberName={member_id}/>
-              </Grid>
-              
-                <Grid item xs={12} align="center">
-                    <InnerNavBar project_id={project_id} member_id={member_id}/>    
-                </Grid>
-
-
-
-            </Grid>
-            <Grid container spacing={5}>
-            <Grid item>
-            </Grid>
-            <Grid container justify='center' alignItems='center' spacing={5}>
-              <Grid item xs={8} className={styles.text}>
-                  <Typography variant="h5" className={styles.graphTitle}>Issue Word Count Per Day</Typography>
-                  <BarChart data={graphData} barLabel1={BarChartProperties.issues.label} barColour1={BarChartProperties.issues.barColour}/>
-              </Grid>
-              <Grid item xs={8}>
-                  <TableContainer className={styles.table}>
-                      <Table aria-label="collapsible table">
-                          <TableHead className={styles.header}>
-                                  <TableRow>
-                                      <TableCell className={styles.dateColumn} align='left'>Date</TableCell>
-                                      <TableCell className={styles.issueColumn} align='left'>Issue</TableCell>
-                                      <TableCell className={styles.noteColumn} align='left'>Note</TableCell>
-                                      <TableCell className={styles.dropDownColumn} align='left'/>
-                                  </TableRow>
-                          </TableHead>
-                          <TableBody>
-                              {issues.map((issue) => (
-                                  <Row key={issue.issueId} row={issue}/>
-                              ))}
-                          </TableBody>
-                      </Table>
-                  </TableContainer>
-              </Grid>
-              </Grid>
-            </Grid>
+  return (
+    <Grid container spacing={5} justify="center" alignItems="center">
+      <Grid item xs={12}>
+        <Grid item xs={12}>
+          <Navbar />
         </Grid>
-    )
-}
+        <Grid item xs={12}>
+          <Banner memberName={member_id} />
+        </Grid>
+      </Grid>
 
+      <Grid item xs={12} align="center">
+        <InnerNavBar project_id={project_id} member_id={member_id} />
+      </Grid>
+
+      <Grid className={classes.graph}>
+        <Typography variant="h5" className={classes.graphTitle}>
+          Issue Word Count Per Day
+        </Typography>
+        <BarChart
+          data={graphData}
+          barLabel1={BarChartProperties.issues.label}
+          barColour1={BarChartProperties.issues.barColour}
+          maintainRatio={false}
+        />
+      </Grid>
+
+      <Grid item className={classes.table}>
+        <TableContainer>
+          <Table aria-label="collapsible table">
+            <TableHead>
+              <TableRow className={classes.header}>
+                <TableCell className={classes.headCell} align="left">
+                  Date
+                </TableCell>
+                <TableCell className={classes.headCell} align="left">
+                  Issue
+                </TableCell>
+                <TableCell className={classes.headCell} align="left">
+                  Note
+                </TableCell>
+                <TableCell className={classes.dropDownColumn} align="left" />
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {issues.map((issue) => (
+                <Row key={issue.issueId} row={issue} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default IssueContributionPage;
--- frontend/src/style/CodeContributionPageStyles.js
+++ frontend/src/style/CodeContributionPageStyles.js
@@ -1,29 +1,28 @@
-import {makeStyles} from '@material-ui/core/styles';
+import {makeStyles} from "@material-ui/core/styles";
 
 export const useGraphStyles = makeStyles({
   text: {
-    textAlign: 'center',
-    fontWeight: 'bold',
+    textAlign: "center",
+    fontWeight: "bold",
   },
 
   graph: {
     width: "60vw",
-    height: '30vh',
-    margin: "20px 0 20px 0"
-},   
+    height: "30vh",
+    margin: "20px 0 20px 0",
+  },
 
-table: {
-  margin: "0 auto 0 auto",
-  width: "90%"
-},
+  table: {
+    margin: "0 auto 0 auto",
+    width: "90%",
+  },
 });
 
 export const useTableStyles = makeStyles({
-  
-  banner:{
-    background: '#d1d0ff',
-    font: 'bold',
-    fontSize: '1.2rem'
+  banner: {
+    background: "#d1d0ff",
+    fontWeight: "bold",
+    fontSize: "1.2rem",
   },
 });
 
@@ -33,29 +32,28 @@ export const usePaginationStyle = makeStyles((theme) => ({
     marginRight: theme.spacing(65),
   },
   icons: {
-    backgroundColor: 'none',
-  }
+    backgroundColor: "none",
+  },
 }));
 
 export const useRowStyles = makeStyles({
   root: {
-    '& > *': {
-      borderBottom: 'unset',
+    "& > *": {
+      borderBottom: "unset",
     },
   },
   cell: {
     width: 300,
   },
-  mrIcon:{
-    color: 'white',
-    background: '#fc8053',
+  mrIcon: {
+    color: "white",
+    background: "#fc8053",
   },
   cIcon: {
-    color: 'white',
-    background: '#6179ff',
+    color: "white",
+    background: "#6179ff",
   },
   dropDownIcon: {
-    background: 'white',
-  }
+    background: "white",
+  },
 });
-
