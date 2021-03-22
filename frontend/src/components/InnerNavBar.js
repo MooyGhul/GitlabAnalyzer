f539a387d79c4976a019d769013008b87c3cc998
katelynk <katelynk@sfu.ca>
2021-03-22 07:22:40 -0700

    Dynamically link the pages in the inner nav bar

--- frontend/src/components/InnerNavBar.js
+++ frontend/src/components/InnerNavBar.js
@@ -1,23 +1,26 @@
 import React from 'react';  
 import {Link} from "react-router-dom"; 
 function MiniNavBar() {
-  return (
+  
+    let project_id = 2
+    let member_id = 2
+    return (
     <div>
       <p>
-        <Link to="/overview/:project_id/:member_id/codecontribution">
-          Code contribution
+        <Link to={`/overview/${project_id}/${member_id}/codecontribution`}>
+          Code 
         </Link>
       </p>
 
       <p>
-        <Link to="/overview/:project_id/:member_id/commentContribution">
-          Comment contribution
+        <Link to={`/overview/${project_id}/${member_id}/commentcontribution`}>
+          Comment 
         </Link>
       </p>
 
       <p>
-        <Link to="/overview/:project_id/:member_id/issueContribution">
-          Issue contribution
+        <Link to={`/overview/${project_id}/${member_id}/issuecontribution`}>
+          Issue 
         </Link>
       </p>
     </div>
