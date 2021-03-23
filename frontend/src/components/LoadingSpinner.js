dee85806738c60125864bfa454b389ae7c721c28
katelynk <katelynk@sfu.ca>
2021-03-23 13:20:27 -0700

    Move styling to LoadingSpinnerStyle.js

--- frontend/src/components/LoadingSpinner.js
+++ frontend/src/components/LoadingSpinner.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-   position:'fixed',
-   height: '100%',
-   width: '100%',
-   top:'0',
-   left:'0',
-   background:'#f8f8f8ad'
-  },
-  spinner:{
-    top: '50%',
-    left: '50%',
-    zIndex: '1000',
-    position: 'absolute',
-
-  }
-}));
+import useStyles from '../style/LoadingSpinnerStyle'
 
 const LoadingSpinner = () => {
   const classes = useStyles();
