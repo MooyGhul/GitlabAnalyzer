1405d94f02041b78e392b2a37f3e9090263d764b
katelynk <katelynk@sfu.ca>
2021-03-25 14:15:21 -0700

    Loda a project before asking for members

--- frontend/src/ProjectInfoPage/ProjectInfoPage.js
+++ frontend/src/ProjectInfoPage/ProjectInfoPage.js
@@ -4,17 +4,23 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import AllProjectInfo from "./AllProjectInfo";
-
+ 
 function ProjectInfoPage(props) {
   const location = useLocation();
   const projectID = location.state.id;
   const projectName = location.state.projectName;
 
   const [members, setMembers] = useState([]);
-  
+   
   useEffect(() => {
+
+    const loadProject = async() => {
+      await axios.post(process.env.NODE_ENV === 'development' ?         
+      `${process.env.REACT_APP_DEVHOST}/project/${projectID}/load` :
+      `/project/${projectID}/load`)
+    }
     const fetchData = async () => {
-      const result = await axios.get(
+        const result = await axios.get(
           process.env.NODE_ENV === 'development' ?
               `${process.env.REACT_APP_DEVHOST}/project/${projectID}/members` :
               `/project/${projectID}/members`
@@ -26,11 +32,33 @@ function ProjectInfoPage(props) {
       else{
         setMembers(result.data);
       }
-    };
+    }; 
+    loadProject();
     fetchData();
   }, [projectID]);
   
+  
 
+  
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //       const result = await axios.get(
+  //         process.env.NODE_ENV === 'development' ?
+  //             `${process.env.REACT_APP_DEVHOST}/project/${projectID}/members` :
+  //             `/project/${projectID}/members`
+  //     );
+      
+  //     if (result.data===""){
+  //       setMembers([])
+  //     }
+  //     else{
+  //       setMembers(result.data);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [projectID]);
+   
   return (
     <div>
       <Header pageTitle="Project Overview"/>
