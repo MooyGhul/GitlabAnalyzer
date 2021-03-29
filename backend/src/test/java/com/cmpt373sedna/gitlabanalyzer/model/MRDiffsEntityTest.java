0425cd848463e559d24d1b124fa526d6e3c8835b
Manmeet Singh <msa203@sfu.ca>
2021-03-28 17:45:55 -0700

    fixed failing tests

--- backend/src/test/java/com/cmpt373sedna/gitlabanalyzer/model/MRDiffsEntityTest.java
+++ backend/src/test/java/com/cmpt373sedna/gitlabanalyzer/model/MRDiffsEntityTest.java
@@ -1,10 +1,13 @@
 package com.cmpt373sedna.gitlabanalyzer.model;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,11 +18,8 @@ public class MRDiffsEntityTest {
                 .versionId(110)
                 .MRIid(87)
                 .projectId(2)
-                .headCommitSHA("33e2ee8579fda5bc36accc9c6fbd0b4fefda9e30")
-                .baseCommitSHA("eeb57dffe83deb686a60a71c16c32f71046868fd")
-                .startCommitSHA("eeb57dffe83deb686a60a71c16c32f71046868fd")
                 .createdAt(Instant.parse("2016-07-26T14:44:48.926Z"))
-                .diff("--- /dev/null\n+++ b/LICENSE\n@@ -0,0 +1,21 @@\n+The MIT License (MIT)\n+\n+Copyright (c) 2018 Administrator\n+\n+Permission is hereby granted, free of charge, to any person obtaining a copy\n+of this software and associated documentation files (the \"Software\"), to deal\n+in the Software without restriction, including without limitation the rights\n+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n+copies of the Software, and to permit persons to whom the Software is\n+furnished to do so, subject to the following conditions:\n+\n+The above copyright notice and this permission notice shall be included in all\n+copies or substantial portions of the Software.\n+\n+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n+SOFTWARE.\n")
+                .commits(getCommitDiffs())
                 .build();
 
         String jsonString = new String(this.getClass().getResourceAsStream("/json/gitlabApi/MRDiffs.json").readAllBytes());
@@ -28,6 +28,28 @@ public class MRDiffsEntityTest {
 
         System.out.println(actual.toString());
         assertEquals(expected,actual);
+    }
+    public List<String> getCommitDiffs(){
+        JSONObject commitDiff = new JSONObject();
+        commitDiff.put("diff", "--- a/doc/update/5.4-to-6.0.md\n+++ b/doc/update/5.4-to-6.0.md\n@@ -71,6 +71,8 @@\n sudo -u git -H bundle exec rake migrate_keys RAILS_ENV=production\n sudo -u git -H bundle exec rake migrate_inline_notes RAILS_ENV=production\n \n+sudo -u git -H bundle exec rake gitlab:assets:compile RAILS_ENV=production\n+\n ```\n \n ### 6. Update config files");
+        commitDiff.put("new_path", "doc/update/5.4-to-6.0.md");
+        commitDiff.put("old_path", "doc/update/5.4-to-6.0.md");
+        commitDiff.put("a_mode", "100644");
+        commitDiff.put("b_mode", "100644");
+        commitDiff.put("new_file", false);
+        commitDiff.put("renamed_file", false);
+        commitDiff.put("deleted_file", false);
+        JSONArray n = new JSONArray();
+        n.put(commitDiff);
+        JSONObject j = new JSONObject();
+        j.put("id","33e2ee8579fda5bc36accc9c6fbd0b4fefda9e30");
+        j.put("author","Administrator");
+        j.put("date","2016-07-26T14 :44:29Z");
+        j.put("message","Change year to 2018");
+        j.put("commitDiffs",n);
+        List<String> x = new ArrayList<>();
+        x.add(j.toString());
+        return x;
     }
 }
 
