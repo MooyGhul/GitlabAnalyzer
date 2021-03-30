b2d67f073bacee8542e3eeb5db0f9c21461caabe
katelynk <katelynk@sfu.ca>
2021-03-29 23:59:53 -0700

    Add a loading spinner to the project info page

--- frontend/src/ProjectInfoPage/ProjectInfoPage.js
+++ frontend/src/ProjectInfoPage/ProjectInfoPage.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import AllProjectInfo from "./AllProjectInfo";
+import useFullPageLoader from "../components/useFullPageLoader";
  
 function ProjectInfoPage(props) {
   const location = useLocation();
@@ -11,10 +12,12 @@ function ProjectInfoPage(props) {
   const projectName = location.state.projectName;
 
   const [members, setMembers] = useState([]);
-   
+  const [loader, showLoader, hideLoader] = useFullPageLoader();
+
   useEffect(() => {
 
     const loadProject = async() => {
+      showLoader();
       await axios.post(process.env.NODE_ENV === 'development' ?         
       `${process.env.REACT_APP_DEVHOST}/project/${projectID}/load` :
       `/project/${projectID}/load`)
@@ -34,37 +37,18 @@ function ProjectInfoPage(props) {
       }
     }; 
     loadProject();
-    fetchData();
-  }, [projectID]);
+    fetchData().then(hideLoader());
+     // eslint-disable-next-line
+  }, [projectID]); 
   
-  
-
-  
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //       const result = await axios.get(
-  //         process.env.NODE_ENV === 'development' ?
-  //             `${process.env.REACT_APP_DEVHOST}/project/${projectID}/members` :
-  //             `/project/${projectID}/members`
-  //     );
-      
-  //     if (result.data===""){
-  //       setMembers([])
-  //     }
-  //     else{
-  //       setMembers(result.data);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [projectID]);
-   
   return (
     <div>
       <Header pageTitle="Project Overview"/>
       <WideHeader id={projectID} projectName={projectName} />
       <AllProjectInfo member={members} projectID={projectID} />
+      {loader}
     </div>
+   
   );
 }
 
