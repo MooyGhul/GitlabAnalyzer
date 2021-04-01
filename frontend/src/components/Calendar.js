c1e0086ae2467e01cae5cb04760ae5148c28ae2f
panwangp <richard_pan@sfu.ca>
2021-03-31 22:18:53 -0700

    The layout page is being refacored, right now I cannot cannot test it. Will work on something else until the blocker is removed

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -7,8 +7,24 @@ import {
 } from '@material-ui/pickers';
 import "react-datepicker/dist/react-datepicker.css";
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 225,
+  },
+}));
 
 const Calendar = (props) => {
+  const classes = useStyles();
+
   const startDateChange = (event)=>{
     props.onStartDateChange(event);
   };
@@ -18,6 +34,34 @@ const Calendar = (props) => {
 
   return (
     <div>
+      <form className={classes.container} noValidate>
+        <TextField
+          id="start-date"
+          label="Start Date"
+          type="datetime-local"
+          defaultValue={new Date(props.startDate)}
+          onChange={startDateChange}
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </form>
+      <form className={classes.container} noValidate>
+        <TextField
+          id="end-date"
+          label="End Date"
+          type="datetime-local"
+          defaultValue={new Date(props.endDate)}
+          onChange={endDateChange}
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </form>
+      
+{/* 
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <Grid container justify="space-around">
               <KeyboardDatePicker
@@ -53,7 +97,7 @@ const Calendar = (props) => {
               />
 
             </Grid>
-          </MuiPickersUtilsProvider>
+          </MuiPickersUtilsProvider> */}
   ​  </div>
   );
 }
