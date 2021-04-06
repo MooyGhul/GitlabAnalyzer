2266f9a76a64e420b594ba9584a1402a34c95fe9
gss33 <gss33@sfu.ca>
2021-04-05 18:23:04 -0700

    Add update test for configurations

--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/IterationConfigurationRepository.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/IterationConfigurationRepository.java
@@ -4,7 +4,12 @@ import com.cmpt373sedna.gitlabanalyzer.model.IterationConfigurationEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IterationConfigurationRepository extends CrudRepository<IterationConfigurationEntity, Integer> {
+    @Override
+    Optional<IterationConfigurationEntity> findById(Integer id);
+
     Iterable<IterationConfigurationEntity> findAllByToken(String token);
 }
--- backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/WeightConfigurationEntityRepository.java
+++ backend/src/main/java/com/cmpt373sedna/gitlabanalyzer/repository/WeightConfigurationEntityRepository.java
@@ -3,6 +3,11 @@ package com.cmpt373sedna.gitlabanalyzer.repository;
 import com.cmpt373sedna.gitlabanalyzer.model.WeightConfigurationEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface WeightConfigurationEntityRepository extends CrudRepository<WeightConfigurationEntity, String> {
+import java.util.Optional;
+
+public interface WeightConfigurationEntityRepository extends CrudRepository<WeightConfigurationEntity, Integer> {
+    @Override
+    Optional<WeightConfigurationEntity> findById(Integer id);
+
     Iterable<WeightConfigurationEntity> findAllByToken(String token);
 }
