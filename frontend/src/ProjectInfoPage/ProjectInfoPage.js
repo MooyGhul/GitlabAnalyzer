d8f249e86b7073b0bfd044ccc37ca47d956399c3
katelynk <katelynk@sfu.ca>
2021-04-09 02:03:12 -0700

    Finally fix the loader bug

--- frontend/src/ProjectInfoPage/ProjectInfoPage.js
+++ frontend/src/ProjectInfoPage/ProjectInfoPage.js
@@ -13,9 +13,7 @@ function ProjectInfoPage({
   onProjectLoadedStateChange,
   projectLoaded,
   onNewProjectLoaded,
-  previousProjectId,
-  onDataFetched,
-  dataFetched,
+  previousProjectId, 
 }) {
   const location = useLocation();
   const [members, setMembers] = useState([]);
@@ -24,138 +22,124 @@ function ProjectInfoPage({
   const [comments, setComments] = useState([]);
   const [issues, setIssues] = useState([]);
   const [loader, showLoader, hideLoader] = useFullPageLoader(); 
-  const [noProjectSelected, showErrorPage] = useProjectNotSelected();
+  const [noProjectSelected, showErrorPage] = useProjectNotSelected(); 
   let commitsArray = [];
   let MRsArray = [];
   let commentsArray = [];
   let issuesArray = [];
   const classes = useStyles();
-  const [projectId, setProjectId] = useState(project_id);
-
-  useEffect(() => {
-    showLoader();
-
-    const updateProjectId = () => {
-      if (projectId === -1) {
-        showErrorPage();
-      } else {
-        try {
-          setProjectId(location.state.id);
-        } catch (err) {
-          setProjectId(project_id);
-        }
-      }
-    };
-    console.log("PROJECT IDDDDDD", projectId);
-    console.log("PREVIOUS ID", previousProjectId);
-
-    const loadProject = async () => {
-      console.log("load project was run")
-      console.log("projectLoaded", projectLoaded)
-
-      let projectUrl = `/project/${projectId}/load`;
-      if (process.env.NODE_ENV === "development") {
-        projectUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/load`;
-        await axios.post(projectUrl);
-      }
-      
-      onNewProjectLoaded(projectId);
-      onProjectLoadedStateChange(!projectLoaded);   
-    };
-
-    const fetchData = async () => {
-      let mrUrl = `/project/${projectId}/merge_requests`;
-      let commitUrl = `/project/${projectId}/commits`;
-      let memberUrl = `/project/${projectId}/members`;
-      let commentUrl = `/project/${projectId}/comments`;
-      let issueUrl = `/project/${projectId}/issues`;
-
-      if (process.env.NODE_ENV === "development") {
-        mrUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/commits`;
-        commitUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/merge_requests`;
-        memberUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/members`;
-        commentUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/comments`;
-        issueUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/issues`;
-      }
-      const mrData = await axios.get(mrUrl);
-      const commitData = await axios.get(commitUrl);
-      const memberData = await axios.get(memberUrl);
-      const commentData = await axios.get(commentUrl);
-      const issueData = await axios.get(issueUrl);
-
-      if (memberData.data === "") {
-        setMembers([]);
-      } else {
-        setMembers(memberData.data);
-      }
-      setCommits(commitData.data);
-      setMRs(mrData.data);
-      setComments(commentData.data);
-      setIssues(issueData.data);  
-    };
-    updateProjectId(); 
-
-    if (!projectLoaded) { 
-      console.log("No project was previously added. Loading project.")
-      loadProject(); 
-    }
-
-    if (projectLoaded && previousProjectId === projectId){
-      console.log("A project is loaded.")
-      fetchData().then(hideLoader());
-    }
-
-    if (projectLoaded && previousProjectId !==projectId){
-      console.log("different project is loaded");
-      loadProject().then(fetchData().then)
-      fetchData().then(hideLoader());
-
-    }
-
-
-    // eslint-disable-next-line
-  }, [projectId, projectLoaded]);
-
-
-
-  members.forEach((member) => {
-    let countCommit = 0;
-    let countMR = 0;
-    let countComment = 0;
-    let countIssue = 0;
-
-    commits.forEach((commit) => {
-      if (member === commit.author) {
-        countCommit++;
-      }
-    });
-    commitsArray.push(countCommit);
-
-    MRs.forEach((MR) => {
-      if (member === MR.author) {
-        countMR++;
-      }
-    });
-    MRsArray.push(countMR);
-
-    comments.forEach((comment) => {
-      if (member === comment.commenter) {
-        countComment++;
-      }
-    });
-    commentsArray.push(countComment);
-
-    issues.forEach((issue) => {
-      if (member === issue.author) {
-        countIssue++;
-      }
-    });
-    issuesArray.push(countIssue);
-  });
-
+  const [projectId, setProjectId] = useState(project_id); 
   
+  const updateProjectId = async () => {
+    if (projectId === -1) {
+      showErrorPage();
+    } else {
+      try {
+        setProjectId(location.state.id);
+      } catch (err) {
+        setProjectId(project_id);
+      }
+    }
+  };
 
+  useEffect( () => {
+  const loadProject = async () => { 
+    showLoader();
+    return axios({
+      method: "post",
+      url: (process.env.NODE_ENV === "development"?
+      `${process.env.REACT_APP_DEVHOST}/project/${projectId}/load`:
+      `/project/${projectId}/load`)
+    }).then((response => { 
+      if (response.status === 200){
+        hideLoader();
+        console.log(response);
+      }
+    }))
+    .catch((error)=>{ 
+      hideLoader();
+      console.log("project could not be loaded");
+    });
+  };
+ 
+  
+  const fetchData = async () => {
+        let mrUrl = `/project/${projectId}/merge_requests`;
+        let commitUrl = `/project/${projectId}/commits`;
+        let memberUrl = `/project/${projectId}/members`;
+        let commentUrl = `/project/${projectId}/comments`;
+        let issueUrl = `/project/${projectId}/issues`;
+  
+        if (process.env.NODE_ENV === "development") {
+          mrUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/commits`;
+          commitUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/merge_requests`;
+          memberUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/members`;
+          commentUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/comments`;
+          issueUrl = `${process.env.REACT_APP_DEVHOST}/project/${projectId}/issues`;
+        }
+        const mrData = await axios.get(mrUrl);
+        const commitData = await axios.get(commitUrl);
+        const memberData = await axios.get(memberUrl);
+        const commentData = await axios.get(commentUrl);
+        const issueData = await axios.get(issueUrl); 
+  
+        if (memberData.data === "") {
+          setMembers([]);
+        } else {
+          setMembers(memberData.data);
+        }
+        setCommits(commitData.data);
+        setMRs(mrData.data);
+        setComments(commentData.data);
+        setIssues(issueData.data);  
+  
+        members.forEach((member) => {
+          let countCommit = 0;
+          let countMR = 0;
+          let countComment = 0;
+          let countIssue = 0;
+      
+          commits.forEach((commit) => {
+            if (member === commit.author) {
+              countCommit++;
+            }
+          });
+          commitsArray.push(countCommit);
+      
+          MRs.forEach((MR) => {
+            if (member === MR.author) {
+              countMR++;
+            }
+          });
+          MRsArray.push(countMR);
+      
+          comments.forEach((comment) => {
+            if (member === comment.commenter) {
+              countComment++;
+            }
+          });
+          commentsArray.push(countComment);
+      
+          issues.forEach((issue) => {
+            if (member === issue.author) {
+              countIssue++;
+            }
+          });
+          issuesArray.push(countIssue);
+        });      
+      };
+
+      const loadAndFetchProjectData = async () => {
+        await loadProject();
+        await fetchData();
+      }    
+      loadAndFetchProjectData();
+    
+    // eslint-disable-next-line
+    },[])
+ 
   return (
+    
     <div>
       <div className={classes.body}>
         <div className={classes.barChart}>
