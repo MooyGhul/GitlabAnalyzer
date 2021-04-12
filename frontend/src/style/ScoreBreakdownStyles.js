db72f2da122e73af06e1412c4da04270ee39a674
Richard Fu <rqfu@sfu.ca>
2021-04-12 13:55:18 -0700

    Fix table being hidden under banner

--- frontend/src/style/ScoreBreakdownStyles.js
+++ frontend/src/style/ScoreBreakdownStyles.js
@@ -1,14 +1,24 @@
 import {makeStyles} from "@material-ui/core/styles";
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles((theme) => ({
   container: {
     position: "absolute",
-    top: "0",
-    left: "0"
+    top: "2vh",
+    left: "0",
+    width: "100%",
   },
   table: {
     margin: "0 auto 0 auto",
     width: "90%",
+    [theme.breakpoints.up("lg")]: {
+      marginTop: "30vh",
+    },
+    [theme.breakpoints.down("md")]: {
+      marginTop: "25vh",
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "20vh",
+    },
   },
   banner: {
     background: "#d1d0ff",
@@ -23,4 +33,4 @@ export const useStyles = makeStyles({
   cell: {
     width: 300,
   },
-});
+}));
